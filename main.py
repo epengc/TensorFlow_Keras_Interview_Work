aac34e86f831a6d8a47890d4f8e82fe78f5591d5
epengc <ce.peng@heliolytics.com>
2020-06-12 02:01:22 -0400

    parts of demo_basic

--- main.py
+++ main.py
@@ -12,15 +12,26 @@ from utils.util import *
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 def demo_basic():
+
     args = parse_parameters()
     data_root_orig = os.path.abspath(args.data_root)
     data_root = pathlib.Path(data_root_orig)
     print('Dataset path = {}'.format(data_root))
+
     for item in data_root.iterdir():
         print(item)
     all_images_paths = list(data_root.glob('*/*'))
     print('some_images_paths = {}'.format(all_images_paths[-5:]))
-    
+    all_images_paths = [str(path) for path in all_images_paths]
+    random.shuffle(all_images_paths)
+    image_count = len(all_images_paths)
+    print('image_count = {}'.format(image_count))
+
+    label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+    print('label_name = {}'.format(label_names))
+    label_to_index = dict((name, index) for index, name in enumerate(label_names))
+    print('label_to_index = {}'.format(label_to_index))
+
 if __name__ == '__main__':
     demo_basic()
 
