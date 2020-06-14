1b62910317499f1d9bd013f1b9d24e74ce63b16f
epengc <ce.peng@heliolytics.com>
2020-06-14 13:52:28 -0400

    final version of vdhomework

--- model/mobile_net.py
+++ model/mobile_net.py
@@ -3,6 +3,18 @@
 import tensorflow as tf
 
 def mobile_net(classes=2):
+    '''Kernal function of the classifier
+    
+    For CPU supported TensorFlow 2.2, using MobileNetV2 as backbone in classifier and loading
+    its pretrained weights will make an easier, faster training and reliable for mobile transfer.
+    (If the MobileNetV2 is inefficient, the customer model can be defined here.)
+
+    Args:
+        classes: int number to indicate amount of classes
+    
+    Returns:
+        model: the subclass of keras.Model
+    '''
     mobile_net = tf.keras.applications.MobileNetV2(input_shape=(192,192,3),include_top=False) 
     mobile_net.trainable=False
     model = tf.keras.Sequential([
--- utils/util.py
+++ utils/util.py
@@ -7,7 +7,18 @@ import numpy as np
 import argparse
 
 def parse_parameters():
+    '''Parameters index
+
+    All parameters settings are listed here.
+
+    Args:
+        None
+
+    Returns:
+        args: a parse_args instance.
+    '''
     parser = argparse.ArgumentParser(description='Visual Defense Homework Net')
+
     parser.add_argument('--data_root', default='./data', type=str, metavar='DIR',
                         help='path to dataset for training and validation')
     parser.add_argument('--data_test', default='./test', type=str, metavar='DIR',
@@ -25,19 +36,38 @@ def parse_parameters():
     parser.add_argument('--ratio', default=0.9, type=float, 
                         help='Ratio of training over validation')
     args = parser.parse_args()
+
     return args
 
+
 def preprocess_img(image):
-    image = tf.image.decode_png(image,channels=3)
+    '''Decode and normalize the image data by using TensorFlow 2.2
+    png and jpeg are acceptable. keras.image.preprocessing.load_img is another choice
+    '''
+    image = tf.image.decode_png(image,channels=3) 
     image = tf.image.resize(image,[192,192], preserve_aspect_ratio=False) #resize images
     image /= 255.0 # normalization 
     return image
 
+
 def load_and_preprocess_image(path):
+    '''Load the image file using TensorFlow 2.2'''
     image = tf.io.read_file(path)
     return preprocess_img(image)
 
+
 def prepare_train_dataset(images_paths, images_labels, num_parallel_calls):
+    '''Prepare the dataset for training
+    Training data can be orgnized in flip-up, random shuffle, cropped and batch_size 
+
+    Args:
+        images_paths: image paths str in list
+        images_labels: class label for each image
+        num_parallel_calls: backend buffer setting
+
+    Return:
+        train_ds: [batch_size, 192, 192, 3] TensorFlow Dataset instance
+    '''
     args = parse_parameters()
     path_ds = tf.data.Dataset.from_tensor_slices(images_paths)
     image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=num_parallel_calls)
@@ -49,7 +79,18 @@ def prepare_train_dataset(images_paths, images_labels, num_parallel_calls):
     train_ds = train_ds.prefetch(buffer_size=num_parallel_calls)
     return train_ds
 
+
 def prepare_val_dataset(images_paths, images_labels, num_parallel_calls):
+    '''Prepare the dataset for validation
+
+    Args:
+        images_paths: image paths str in list
+        images_labels: class label for each image
+        num_parallel_calls: backend buffer setting
+
+    Return:
+        val_ds: [batch_size, 192, 192, 3] TensorFlow Dataset instance
+    '''
     args = parse_parameters()
     path_ds = tf.data.Dataset.from_tensor_slices(images_paths)
     image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=num_parallel_calls)
@@ -58,7 +99,17 @@ def prepare_val_dataset(images_paths, images_labels, num_parallel_calls):
     val_ds = image_label_ds.batch(args.val_batch_size)
     return val_ds
 
+
 def prepare_test_dataset(images_paths, num_parallel_calls):
+    '''Prepare the dataset for test
+
+    Args:
+        images_paths: image paths str in list
+        num_parallel_calls: backend buffer setting
+
+    Return:
+        test_ds: [batch_size, 192, 192, 3] TensorFlow Dataset instance
+    '''
     args = parse_parameters()
     path_ds = tf.data.Dataset.from_tensor_slices(images_paths)
     image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=num_parallel_calls)
